91cc448ec30b22376fd8fb2795246618fb08697c
Khalil <h.sultanoff@yandex.ru>
2025-05-29 15:04:54 +0300

    add auto-creating admin-user

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,16 @@ done
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Creating superuser if not exists..."
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    print("Superuser 'admin' created.")
+else:
+    print("Superuser 'admin' already exists.")
+END
+
 echo "Running server..."
 exec "$@"
